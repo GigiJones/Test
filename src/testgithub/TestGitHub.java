dbf09c5f63d3e88957cacdcfae4cacbd256364bf
Gabrielle Jones-154003549 <154003549@vusdnoc.vusd.k12.ca.us>
2017-10-30 10:08:24 -0700

    added a new file

--- src/testgithub/TestGitHub.java
+++ src/testgithub/TestGitHub.java
@@ -16,7 +16,8 @@ public class TestGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("hola");
     }
     
 }
